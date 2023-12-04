8128bbcaafec34cdadc8e6036bd8aebb24cc7535
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-12-04 19:54:18 +0900

    fix python 두 수의 차
    제한사항 num1,2가 -50000이상 50000이하

--- programmers/두 수의 차/python.py
+++ programmers/두 수의 차/python.py
@@ -1,2 +1,9 @@
 def solution(num1, num2):
+    '''
+    if not (-50000 <= num1 <= 50000 and -50000 <= num2 <= 50000):
+        exit()
+    '''
+    valid_range = range(-50000, 50001)
+    if not(num1 in valid_range and num2 in valid_range):
+        exit()
     return (num1 - num2)
