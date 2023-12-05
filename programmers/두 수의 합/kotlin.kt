b0c1a91ec2818800852ea4bb154d4ff7f916c5c5
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-12-05 18:19:31 +0900

    add kotlin 두 수의 합
    kotlinic 하게 접근&sum 함수 사용

--- programmers/두 수의 합/kotlin.kt
+++ programmers/두 수의 합/kotlin.kt
@@ -1,6 +1,16 @@
-fun solution(num1: Int, num2: Int): Int {
-    if (!(num1 in -50000..50000 && num2 in -50000..50000)) {
-        return -1
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        /*
+        if (!(num1 in -50000..50000 && num2 in -50000..50000)) {
+            return -1
+        }
+        return num1 + num2
+        */
+        return if (num1 in -50000..50000 && num2 in -50000..50000) {
+//        num1 + num2
+            listOf(num1, num2).sum()
+        } else {
+            -1
+        }
     }
-    return num1 + num2
 }
