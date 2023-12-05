e5b9f9b9c8d919a07476b85977c02e703e0c6901
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-12-05 17:58:51 +0900

    fix python 숫자 비교하기
    pythonic하게 변경

--- programmers/숫자 비교하기/python.py
+++ programmers/숫자 비교하기/python.py
@@ -1,8 +1,11 @@
 def solution(num1, num2):
     valid_range = range(0, 10001)
-    if not(num1 in valid_range and num2 in valid_range):
+    if not (num1 in valid_range and num2 in valid_range):
         exit()
+    '''
     if num1 == num2:
         return 1
     else:
         return -1
+    '''
+    return 1 if num1 == num2 else -1
