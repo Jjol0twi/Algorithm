a9bb08390920f58df877950e40677ab57f2f1e15
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-12-05 18:02:50 +0900

    remote python  두 수의 곱
    remote file with fleet idea

--- programmers/두 수의 곱/python.py
+++ programmers/두 수의 곱/python.py
@@ -1,9 +1,9 @@
 def solution(num1, num2):
-    '''
+    """
     if not (0 <= num1 <= 100 and 0 <= num2 <= 100):
         exit()
-    '''
+    """
     valid_range = range(0, 101)
-    if not(num1 in valid_range and num2 in valid_range):
+    if not (num1 in valid_range and num2 in valid_range):
         exit()
     return num1 * num2
