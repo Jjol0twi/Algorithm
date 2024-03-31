6dc50e53f52ca3839110c90599f6b2b2ab1fecf4
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2024-03-31 12:34:23 +0900

    reformat beakjoon 9711 python

--- beakjoon/9711/python.py
+++ beakjoon/9711/python.py
@@ -1,16 +1,18 @@
+import sys
+sys.setrecursionlimit(10**6)
 def fibonacci(n: int):
     if n < 1:
         return []
-    fib_list=[1,1]  # 재귀함수 runtime error로 for문으로 교체
-    for i in range(2, n):
-        fib_list.append(fib_list[-1] + fib_list[-2])
-    return fib_list
-    # if n == 1 or n==2:
-    #     return [1, 1]
-    # else:
-    #     fib_list = fibonacci(n - 1)
+    # fib_list=[1,1]  # 재귀함수 runtime error로 for문으로 교체
+    # for i in range(2, n):
     #     fib_list.append(fib_list[-1] + fib_list[-2])
-    #     return fib_list
+    # return fib_list
+    if n == 1 or n==2:
+        return [1, 1]
+    else:
+        fib_list = fibonacci(n - 1)
+        fib_list.append(fib_list[-1] + fib_list[-2])
+        return fib_list
 
 testNum = int(input())
 testCase = []
