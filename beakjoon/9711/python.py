83b43e624890ade063b8976d180f05a2b079a8cb
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2024-03-31 12:27:24 +0900

    reformat beakjoon 9711 python

--- beakjoon/9711/python.py
+++ beakjoon/9711/python.py
@@ -1,14 +1,22 @@
 def fibonacci(n: int):
-    if n<1:
-        return 0
-    if n==1 or n==2:
-        return 1
-    return fibonacci(n-2)+fibonacci(n-1)
+    if n < 1:
+        return []
+    fib_list=[1,1]  # 재귀함수 runtime error로 for문으로 교체
+    for i in range(2, n):
+        fib_list.append(fib_list[-1] + fib_list[-2])
+    return fib_list
+    # if n == 1 or n==2:
+    #     return [1, 1]
+    # else:
+    #     fib_list = fibonacci(n - 1)
+    #     fib_list.append(fib_list[-1] + fib_list[-2])
+    #     return fib_list
 
 testNum = int(input())
 testCase = []
 for i in range(testNum):
     testCase.append(list(map(int, input().split())))
-result=[]
+max_fibonacci_index = max(i[0] for i in testCase)
+fibonacci_list = fibonacci(max_fibonacci_index)
 for i in range(len(testCase)):
-    print("Case #{}: {}".format(i+1,fibonacci(testCase[i][0])%testCase[i][1]))
+    print("Case #{}: {}".format(i+1,fibonacci_list[testCase[i][0]-1]%testCase[i][1]))
