ee3b369b3d13f0babf88fd8c1f6bac998d20b828
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2024-04-14 10:23:17 +0900

    remote beakjoon 18111 python but time out

--- beakjoon/18111/python.py
+++ beakjoon/18111/python.py
@@ -1,14 +1,25 @@
-n, m, b = map(int, input().split())
-elev = [list(map(int, input().split()[:m])) for _ in range(n)]
-to_elev = sum(sum(elev,[]))
-ro_elev = round(to_elev/(n*m))
-while ro_elev*(n*m)+b>to_elev:
-    print("ㅋ")
-    ro_elev-=1
-    if to_elev%(n*m)==0:
-        b+=(n*m)
-    else:
-        b+=to_elev%(n*m)
-    print(n,m,b,elev,to_elev,ro_elev)
-to_elev-=b
-print(b)
+import sys
+from collections import Counter
+
+answer = [0,0]
+n, m, b = map(int, sys.stdin.readline().split()) # 입력 1
+elev = [list(map(int, sys.stdin.readline().split()[:m])) for _ in range(n)]  # 입력 2
+min_height = min(map(min, elev))    # 최소 높이
+max_height = max(map(max, elev))    # 최대 높이
+total_blocks = sum(sum(elev,[]))    # 설치된 블록 총 수
+dic_box = Counter() # 입력 받은 수 dic으로 변경 k(숫자):v(숫자 중복 개수)
+for i in elev:
+    dic_box += Counter(i)
+for i in range(min_height,max_height+1):
+    if i*(n*m) <= sum(sum(elev,[]))+b:
+        p = [0,i]
+        for k, v in dic_box.items():
+            if k!=i:
+                j = k-i
+                if j < 0:
+                    p[0]+=abs(j)*v
+                else:
+                    p[0]+=j*2*v
+        if answer[0]>=p[0] or answer[0]==0:
+            answer=p
+print(answer[0],answer[1])
