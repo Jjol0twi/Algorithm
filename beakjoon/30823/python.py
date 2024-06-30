4abb41d086c84f02c94c531e56ad13de986b79dd
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2024-06-30 11:24:30 +0900

    add beakjoon 30823 python

---
+++ beakjoon/30823/python.py
@@ -0,0 +1,10 @@
+from sys import stdin
+n, k = map(int, stdin.readline().strip().split())
+s = list(stdin.readline().strip())
+rs = s[:(k-1)]
+del s[:(k-1)]
+if (n-k)%2==0:
+    rs.reverse()
+    print(''.join([*s,*rs]))
+else:
+    print(''.join([*s,*rs]))
