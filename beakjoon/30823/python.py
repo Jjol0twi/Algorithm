3bc4dd78ad7cd62f00f4a299cfefade70081ffc5
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2024-07-07 11:57:40 +0900

    add comments beakjoon 30823 python

--- beakjoon/30823/python.py
+++ beakjoon/30823/python.py
@@ -1,8 +1,10 @@
 from sys import stdin
-n, k = map(int, stdin.readline().strip().split())
-s = list(stdin.readline().strip())
-rs = s[:(k-1)]
-del s[:(k-1)]
+# 문제 규칙을 파악해서 푼 문제
+# 앞에서부터 k만큼 뒤로 보내되 (n-k)+₩
+n, k = map(int, stdin.readline().strip().split())   # n, k 입력받기
+s = list(stdin.readline().strip())  # s list로 받기
+rs = s[:(k-1)]  # 앞에서부터 k만큼 rs에 저
+del s[:(k-1)]   #
 if (n-k)%2==0:
     rs.reverse()
 print(''.join([*s,*rs]))
